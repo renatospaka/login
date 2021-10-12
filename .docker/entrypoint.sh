64a3e0ba2800106f2269cd013bd8503942cc55cb
Renato Spakauskas <renatospaka@gmail.com>
2021-10-12 19:12:29 -0300

    refactoring project

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,9 +4,10 @@
 #   cp .env.example .env
 # fi
 
-npm init -y
-npm install standard nodemon jest -D
-npm install express mongoose 
-# npm install lint-staged husky@next -D
-# npm install
-npm run start
+# npm init -y
+# npm install standard nodemon jest -D
+# npm install express mongoose 
+# # npm install lint-staged husky@next -D
+npm install
+# npm run start
+npm run test
