200bc7b348e9cd89f9f260c91902abff3c2a5fdf
Renato Spakauskas <renatospaka@gmail.com>
2021-10-15 07:38:29 -0300

    refactor errors to new folder structure

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,11 +1,7 @@
-const InvalidParamError = require('./invalid-param-error')
-const MissingParamError = require('./missing-param-error')
 const UnauthorizedError = require('./unauthorized-error')
 const ServerError = require('./server-error')
 
 module.exports = {
-  InvalidParamError,
-  MissingParamError,
   UnauthorizedError,
   ServerError
 }
